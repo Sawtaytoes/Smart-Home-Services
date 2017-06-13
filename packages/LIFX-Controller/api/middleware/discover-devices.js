97db79de4b1a31f9286cb3906c6a0e357c930938
Kevin Ghadyani <Saturn2888@gmail.com>
2017-06-13 02:03:46 -0500

    Running config update in parallel to LAN protocol discovery

--- packages/LIFX-Controller/api/middleware/discover-devices.js
+++ packages/LIFX-Controller/api/middleware/discover-devices.js
@@ -5,11 +5,12 @@ const DURATION = 30000
 
 module.exports = (lifxClient, lifxConfig) => _ => {
 	logger('Command: Discover Devices')
+
+	lifxConfig.update()
 	lifxClient.startDiscovery()
 
 	new Promise(resolve => setTimeout(resolve, DURATION))
 	.then(() => lifxClient.stopDiscovery())
-	.then(lifxConfig.update)
 	.then(() => logger('Discover Devices: Complete'))
 	.catch(err => console.error('Discover Devices:', err))
 }
