4c75ec1dd6c627061367791c48b1446ffbbbd2fd
Kevin Ghadyani <Saturn2888@gmail.com>
2017-08-01 01:31:09 -0500

    Updated `isLightOn` function name in toggle-lights

--- packages/LIFX-Controller/api/middleware/toggle-lights.js
+++ packages/LIFX-Controller/api/middleware/toggle-lights.js
@@ -9,7 +9,7 @@ const DURATION = 500
 const isLightOnline = Boolean
 const getLightByName = lifxClient => name => lifxClient.light(name)
 
-const isLightOn = lights => (
+const isOneOrMoreLightsOn = lights => (
 	lights.some(({ settings: { power } }) => power === POWERED_ON)
 )
 
@@ -22,7 +22,7 @@ const turnOnLight = changeLightPower('on')
 
 const toggleLights = lights => (
 	Promise.all(
-		isLightOn(lights)
+		isOneOrMoreLightsOn(lights)
 		? lights.map(turnOffLight)
 		: lights.map(turnOnLight)
 	)
