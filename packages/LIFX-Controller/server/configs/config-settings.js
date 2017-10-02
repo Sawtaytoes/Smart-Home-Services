190526d06a56bd31e507ae1aa35c8904be391a1c
Kevin Ghadyani <Saturn2888@gmail.com>
2017-10-02 01:14:49 -0500

    Removed unnecessary comment from config-settings

--- packages/LIFX-Controller/server/configs/config-settings.js
+++ packages/LIFX-Controller/server/configs/config-settings.js
@@ -9,7 +9,6 @@ try {
 const configDefaults = {
 	env: 'production',        // Can be 'development' or 'production'.
 
-	//- Server (use this in your browser)
 	protocol: 'http',         // Using `https` requires valid certificates.
 	hostname: '0.0.0.0',      // Can be 0.0.0.0 for binding to all ports.
 	port: 3000,               // Port of webserver.
