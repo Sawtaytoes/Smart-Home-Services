6e7125d976a0bb24ca370b6ec6cedae8a6ea40c1
Kevin Ghadyani <Saturn2888@gmail.com>
2018-08-16 02:11:48 -0500

    Blacklisted ADD_GROUP

--- packages/LIFX-Controller/redux/actionsBlacklist.js
+++ packages/LIFX-Controller/redux/actionsBlacklist.js
@@ -1,9 +1,14 @@
 const {
+	ADD_GROUP,
+} = require('$redux/groups/actions')
+
+const {
 	ADD_LIFX_NETWORK_LIGHT,
 	REMOVE_LIFX_NETWORK_LIGHT,
 } = require('$redux/lifxNetwork/actions')
 
 module.exports = [
+	ADD_GROUP,
 	ADD_LIFX_NETWORK_LIGHT,
 	REMOVE_LIFX_NETWORK_LIGHT,
 ]
