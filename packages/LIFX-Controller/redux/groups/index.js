e0b0b144a1661b665d76b6eed6a909249e9a7f95
Kevin Ghadyani <Saturn2888@gmail.com>
2018-08-16 00:20:36 -0500

    Removed unnecessary comments from Redux groups

--- packages/LIFX-Controller/redux/groups/index.js
+++ packages/LIFX-Controller/redux/groups/index.js
@@ -2,7 +2,6 @@ const { combineEpics } = require('redux-observable')
 const { combineReducers } = require('redux')
 
 const addGroupsEpic = require('./addGroupsEpic')
-// const groupsCountEpic = require('./groupsCountEpic')
 const groupsListReducer = require('./groupsListReducer')
 const lightGroupsListReducer = require('./lightGroupsListReducer')
 const requestsEpic = require('./requestsEpic')
@@ -11,7 +10,6 @@ const toggleGroupEpic = require('./toggleGroupEpic')
 const groupsEpic = (
 	combineEpics(
 		addGroupsEpic,
-		// groupsCountEpic,
 		requestsEpic,
 		toggleGroupEpic,
 	)
