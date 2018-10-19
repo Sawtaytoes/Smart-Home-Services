7e6261b807d3eb52a553a4e317759d32399e265b
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-18 23:08:34 -0500

    Formatted toggleGroupEpic

--- packages/LIFX-Controller/redux/groups/toggleGroupEpic.js
+++ packages/LIFX-Controller/redux/groups/toggleGroupEpic.js
@@ -35,134 +35,135 @@ const turnOffLight = changeLightPower('off')
 const turnOnLight = changeLightPower('on')
 
 const toggleGroupEpic = (
-	(action$, state$) => (
-		action$
-		.pipe(
-			ofType(TOGGLE_GROUP),
-			mergeMap(({ groupName }) => (
-				stateSelector({
-					props: { groupName },
-					selector: lightIdsSelector,
-					state$,
-				})
-				.pipe(
-					tap(lightIds => (
-						(
-							!lightIds
-							|| !lightIds.size
-						)
-						? (
-							console
-							.warn(
-								chalk
-								.redBright(
-									'[MISSING GROUP]'
-								)
-								.concat(' ')
-								.concat(
-									chalk
-									.bgRed(
-										groupName
-									)
-								)
-							)
-						)
-						: (
-							console
-							.info(
-								chalk
-								.greenBright(
-									'[TOGGLE GROUP]'
-								)
-								.concat(' ')
-								.concat(
-									chalk
-									.bgGreen(
-										groupName
-									)
-								)
-							)
-						)
-					)),
-					takeUntil(
-						action$
-						.pipe(
-							ofType(TOGGLE_GROUP),
-							filter(action => (
-								action.groupName === groupName
-							))
-						)
-					),
-				)
-			)),
-			filter(Boolean),
-			map(lightIds => (
-				Array
-				.from(lightIds)
-			)),
-			mergeMap(lightIds => (
-				forkJoin(
-					...(
-						lightIds
-						.map(lightId => (
-							// TODO: NEED TO HANDLE ERRORS IN HERE
-							// OTHERWISE ONE ERROR WILL KILL THE ENTIRE FORKJOIN
-							stateSelector({
-								props: { lightId },
-								selector: networkLightSelector,
-								state$,
-							})
-							.pipe(
-								filter(Boolean),
-								switchMap(light => (
-									bindNodeCallback(
-										light
-										.getPower
-										.bind(light)
-									)()
-									.pipe(
-										map(power => ({
-											light,
-											power,
-										}))
-									)
-								)),
-							)
-						))
-						// from(lightIds)
-						// .pipe(
-						// 	map(lightId => (
-						// 		stateSelector({
-						// 			props: { lightId },
-						// 			selector: networkLightSelector,
-						// 			state$,
-						// 		})
-						// 	)),
-						// )
+	action$,
+	state$,
+) => (
+	action$
+	.pipe(
+		ofType(TOGGLE_GROUP),
+		mergeMap(({ groupName }) => (
+			stateSelector({
+				props: { groupName },
+				selector: lightIdsSelector,
+				state$,
+			})
+			.pipe(
+				tap(lightIds => (
+					(
+						!lightIds
+						|| !lightIds.size
 					)
+					? (
+						console
+						.warn(
+							chalk
+							.redBright(
+								'[MISSING GROUP]'
+							)
+							.concat(' ')
+							.concat(
+								chalk
+								.bgRed(
+									groupName
+								)
+							)
+						)
+					)
+					: (
+						console
+						.info(
+							chalk
+							.greenBright(
+								'[TOGGLE GROUP]'
+							)
+							.concat(' ')
+							.concat(
+								chalk
+								.bgGreen(
+									groupName
+								)
+							)
+						)
+					)
+				)),
+				takeUntil(
+					action$
+					.pipe(
+						ofType(TOGGLE_GROUP),
+						filter(action => (
+							action.groupName === groupName
+						))
+					)
+				),
+			)
+		)),
+		filter(Boolean),
+		map(lightIds => (
+			Array
+			.from(lightIds)
+		)),
+		mergeMap(lightIds => (
+			forkJoin(
+				...(
+					lightIds
+					.map(lightId => (
+						// TODO: NEED TO HANDLE ERRORS IN HERE
+						// OTHERWISE ONE ERROR WILL KILL THE ENTIRE FORKJOIN
+						stateSelector({
+							props: { lightId },
+							selector: networkLightSelector,
+							state$,
+						})
+						.pipe(
+							filter(Boolean),
+							switchMap(light => (
+								bindNodeCallback(
+									light
+									.getPower
+									.bind(light)
+								)()
+								.pipe(
+									map(power => ({
+										light,
+										power,
+									}))
+								)
+							)),
+						)
+					))
+					// from(lightIds)
+					// .pipe(
+					// 	map(lightId => (
+					// 		stateSelector({
+					// 			props: { lightId },
+					// 			selector: networkLightSelector,
+					// 			state$,
+					// 		})
+					// 	)),
+					// )
 				)
-			)),
-			// tap(console.log),
-			tap(t => console.log(t.length)),
-			mergeMap(lightsInGroup => (
-				isLightOnInGroup(lightsInGroup)
-				? (
-					lightsInGroup
-					.map(turnOffLight)
-				)
-				: (
-					lightsInGroup
-					.map(turnOnLight)
-				)
-			)),
-			// tap(console.log),
-			mergeMap(light$ => (
-				light$
-			)),
-			// TODO: LOG WHEN FINISHED WITH ALL
-			// tap(console.log),
-			ignoreElements(),
-		)
+			)
+		)),
+		// tap(console.log),
+		tap(t => console.log(t.length)),
+		mergeMap(lightsInGroup => (
+			isLightOnInGroup(lightsInGroup)
+			? (
+				lightsInGroup
+				.map(turnOffLight)
+			)
+			: (
+				lightsInGroup
+				.map(turnOnLight)
+			)
+		)),
+		// tap(console.log),
+		mergeMap(light$ => (
+			light$
+		)),
+		// TODO: LOG WHEN FINISHED WITH ALL
+		// tap(console.log),
+		ignoreElements(),
 	)
 )
 
