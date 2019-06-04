6d900e5642a74cdf6396d5cc8c2ebf5feda06702
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-03 19:17:34 -0500

    Removed scene information from lights/actions

--- packages/LIFX-Controller/redux/lights/actions.js
+++ packages/LIFX-Controller/redux/lights/actions.js
@@ -1,6 +1,5 @@
 const ADD_HTTP_API_LIGHTS = 'LIGHTS::ADD_HTTP_API_LIGHTS'
 const ADD_NETWORK_LIGHTS = 'LIGHTS::ADD_NETWORK_LIGHTS'
-const ADD_SCENE = 'LIGHTS::ADD_SCENE'
 const REMOVE_NETWORK_LIGHT = 'LIGHTS::REMOVE_NETWORK_LIGHT'
 
 const addHttpApiLights = (
@@ -24,22 +23,11 @@ const removeNetworkLight = (
 	type: REMOVE_NETWORK_LIGHT,
 })
 
-const addScene = ({
-	namespace,
-	scene,
-}) => ({
-	namespace,
-	scene,
-	type: ADD_SCENE,
-})
-
 module.exports = {
 	ADD_HTTP_API_LIGHTS,
 	ADD_NETWORK_LIGHTS,
-	ADD_SCENE,
 	addHttpApiLights,
 	addNetworkLights,
-	addScene,
 	REMOVE_NETWORK_LIGHT,
 	removeNetworkLight,
 }
