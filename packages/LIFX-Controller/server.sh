a250916d9812447b5f7c594a9aa36cf3b34e01f7
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-04 03:57:52 -0500

    Added missing server start files

---
+++ packages/LIFX-Controller/server.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+numberOfServers=$1
+
+if [[ ! $numberOfServers ]]; then
+	numberOfServers=0
+fi
+
+pm2 start app.js -i $numberOfServers --name ${PWD##*/} -- server
