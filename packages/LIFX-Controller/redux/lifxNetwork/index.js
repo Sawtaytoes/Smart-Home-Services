1638018a985cea83d304997587f772a21ebac0e2
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-04 04:16:09 -0500

    Fixed a capitalization error

--- packages/LIFX-Controller/redux/lifxNetwork/index.js
+++ packages/LIFX-Controller/redux/lifxNetwork/index.js
@@ -1,14 +1,14 @@
 const { combineEpics } = require('redux-observable')
 const { combineReducers } = require('redux')
 
-const addlifxNetworkClientEpic = require('./addlifxNetworkClientEpic')
+const addLifxNetworkClientEpic = require('./addLifxNetworkClientEpic')
 const initializeLifxNetworkClientEpic = require('./initializeLifxNetworkClientEpic')
 const lifxNetworkClientReducer = require('./lifxNetworkClientReducer')
 const lifxNetworkDiscoveryEpic = require('./lifxNetworkDiscoveryEpic')
 
 const lifxNetworkEpic = (
 	combineEpics(
-		addlifxNetworkClientEpic,
+		addLifxNetworkClientEpic,
 		initializeLifxNetworkClientEpic,
 		lifxNetworkDiscoveryEpic,
 	)
