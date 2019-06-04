77ef2de325294edd313e909d9e3b9f543cb96239
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-04 02:44:09 -0500

    Refactored code styling in lifxNetworkDiscoveryEpic

--- packages/LIFX-Controller/redux/lifxNetwork/lifxNetworkDiscoveryEpic.js
+++ packages/LIFX-Controller/redux/lifxNetwork/lifxNetworkDiscoveryEpic.js
@@ -2,6 +2,8 @@ const { fromEvent, merge, of } = require('rxjs')
 const { map, switchMap } = require('rxjs/operators')
 const { ofType } = require('redux-observable')
 
+const catchEpicError = require('$redux/utils/catchEpicError')
+
 const {
 	ADD_LIFX_NETWORK_CLIENT,
 	addLifxNetworkLight,
@@ -10,43 +12,44 @@ const {
 } = require('./actions')
 
 const lifxNetworkDiscoveryEpic = (
-	action$ => (
-		action$
-		.pipe(
-			ofType(ADD_LIFX_NETWORK_CLIENT),
-			switchMap(({ lifxNetworkClient }) => (
-				merge(
-					(
-						fromEvent(
-							lifxNetworkClient,
-							'light-new',
-						)
-						.pipe(
-							map(addLifxNetworkLight)
-						)
-					),
-					(
-						fromEvent(
-							lifxNetworkClient,
-							'light-offline',
-						)
-						.pipe(
-							map(removeLifxNetworkLight)
-						)
-					),
-					(
-						fromEvent(
-							lifxNetworkClient,
-							'light-online',
-						)
-						.pipe(
-							map(addLifxNetworkLight)
-						)
-					),
-					of(startNetworkListeners()),
-				)
-			)),
-		)
+	action$,
+) => (
+	action$
+	.pipe(
+		ofType(ADD_LIFX_NETWORK_CLIENT),
+		switchMap(({ lifxNetworkClient }) => (
+			merge(
+				(
+					fromEvent(
+						lifxNetworkClient,
+						'light-new',
+					)
+					.pipe(
+						map(addLifxNetworkLight)
+					)
+				),
+				(
+					fromEvent(
+						lifxNetworkClient,
+						'light-offline',
+					)
+					.pipe(
+						map(removeLifxNetworkLight)
+					)
+				),
+				(
+					fromEvent(
+						lifxNetworkClient,
+						'light-online',
+					)
+					.pipe(
+						map(addLifxNetworkLight)
+					)
+				),
+				of(startNetworkListeners()),
+			)
+		)),
+		catchEpicError(),
 	)
 )
 
