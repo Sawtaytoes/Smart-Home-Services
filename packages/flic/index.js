4eccb4ecc735fb147472b4d2d62f5f529beccdcf
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-30 01:50:05 -0500

    Added `connections` to Flic export

--- packages/flic/index.js
+++ packages/flic/index.js
@@ -3,4 +3,5 @@ require('better-module-alias')(__dirname)
 
 module.exports = {
 	buttonPresses: require('$redux/buttonPresses').buttonPresses,
+	connections: require('$redux/connections').connections,
 }
