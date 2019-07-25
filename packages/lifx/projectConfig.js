d302cee247c9088ed67aca9559022b97954404bb
Kevin Ghadyani <Saturn2888@gmail.com>
2019-07-24 22:13:49 -0500

    Tried more-sane defaults for messageHandlerTimeout

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -2,7 +2,7 @@ module.exports = {
 	applicationName: 'lifxController',
 	lifxApiAddress: 'https://api.lifx.com/v1/',
 	lifxLanClient: {
-		messageHandlerTimeout: 50,
-		resendMaxTimes: 10,
+		messageHandlerTimeout: 100,
+		resendMaxTimes: 5,
 	},
 }
