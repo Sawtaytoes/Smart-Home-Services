ea96f14dfe890aa13858a49b08a82452aee3f79a
Kevin Ghadyani <Saturn2888@gmail.com>
2019-07-25 17:13:32 -0500

    Updated max resend and timeout for LIFX to a more-reasonable value

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -2,7 +2,7 @@ module.exports = {
 	applicationName: 'lifxController',
 	lifxApiAddress: 'https://api.lifx.com/v1/',
 	lifxLanClient: {
-		messageHandlerTimeout: 100,
-		resendMaxTimes: 5,
+		messageHandlerTimeout: 500,
+		resendMaxTimes: 3,
 	},
 }
