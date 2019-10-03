d24b4b60a8403db258e03d878fcbff2fceab9eaf
Kevin Ghadyani <Saturn2888@gmail.com>
2019-10-03 03:24:20 -0500

    Added ability to lock and unlock lights by their LIFX ID

--- packages/lifx/redux/lights/actions.js
+++ packages/lifx/redux/lights/actions.js
@@ -1,6 +1,8 @@
 const ADD_HTTP_API_LIGHTS = 'LIGHTS::ADD_HTTP_API_LIGHTS'
 const ADD_NETWORK_LIGHTS = 'LIGHTS::ADD_NETWORK_LIGHTS'
+const LOCK_LIGHTS = 'LIGHTS::LOCK_LIGHTS'
 const REMOVE_NETWORK_LIGHT = 'LIGHTS::REMOVE_NETWORK_LIGHT'
+const UNLOCK_LIGHTS = 'LIGHTS::UNLOCK_LIGHTS'
 
 const addHttpApiLights = (
 	lights,
@@ -16,6 +18,13 @@ const addNetworkLights = (
 	type: ADD_NETWORK_LIGHTS,
 })
 
+const lockLights = (
+	lightIds,
+) => ({
+	lightIds,
+	type: LOCK_LIGHTS,
+})
+
 const removeNetworkLight = ({
 	light,
 }) => ({
@@ -23,11 +32,22 @@ const removeNetworkLight = ({
 	type: REMOVE_NETWORK_LIGHT,
 })
 
+const unlockLights = (
+	lightIds,
+) => ({
+	lightIds,
+	type: UNLOCK_LIGHTS,
+})
+
 module.exports = {
 	ADD_HTTP_API_LIGHTS,
 	ADD_NETWORK_LIGHTS,
 	addHttpApiLights,
 	addNetworkLights,
+	LOCK_LIGHTS,
+	lockLights,
 	REMOVE_NETWORK_LIGHT,
 	removeNetworkLight,
+	UNLOCK_LIGHTS,
+	unlockLights,
 }
