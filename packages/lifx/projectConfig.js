7016093bf9251bcde455dbe436abce976ed8c560
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-24 01:51:06 -0500

    Quick fix to speed up processing of lifxLanClient
    Lights eventually do come on, but it could be now well after I'm done with them. No reason to keep processing on those lights.

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -1,9 +1,8 @@
 module.exports = {
 	applicationName: 'lifxController',
 	lifxApiAddress: 'https://api.lifx.com/v1/',
-	// TEMP. This is not working as expected. Seems to be not waiting long enough.
-	// lifxLanClient: {
-	// 	messageHandlerTimeout: 500,
-	// 	resendMaxTimes: 3,
-	// },
+	lifxLanClient: {
+		messageHandlerTimeout: 2000,
+		resendMaxTimes: 10,
+	},
 }
