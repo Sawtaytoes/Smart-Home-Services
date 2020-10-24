1306e6c8b23d9b74a03a1ce2ebb95bc9eaf2c9fb
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-24 16:06:24 -0500

    Fixed ESLint rule problems in ignoreIfLockedLIghtIds

--- packages/lifx/redux/scenes/utils/ignoreIfLockedLightIds.js
+++ packages/lifx/redux/scenes/utils/ignoreIfLockedLightIds.js
@@ -1,38 +1,42 @@
-const { filter, map, mapTo, mergeMap } = require('rxjs/operators')
-const { of } = require('rxjs')
+const { filter } = require('rxjs/operators')
+// const { filter, map, mapTo, mergeMap } = require('rxjs/operators')
+// const { of } = require('rxjs')
 
-const { selectLockedLightIds } = require('$redux/lights/selectors')
+// const { selectLockedLightIds } = require('$redux/lights/selectors')
 
-const ignoreIfLockedLightIds = ({
-	state$,
-}) => (
-	// TEMP filter. Might end up keeping it like this because commenting this out will give more immediate feedback.
+// TEMP filter. Might end up keeping it like this because commenting this out will give more immediate feedback.
+const ignoreIfLockedLightIds = () => (
 	filter(Boolean)
-	// mergeMap((
-	// 	lightIds,
-	// ) => (
-	// 	of(
-	// 		state$
-	// 		.value
-	// 	)
-	// 	.pipe(
-	// 		map(selectLockedLightIds()),
-	// 		filter((
-	// 			lockedLightIds,
-	// 		) => (
-	// 			!(
-	// 				lightIds
-	// 				.some((
-	// 					lightId,
-	// 				) => (
-	// 					lockedLightIds
-	// 					.has(lightId)
-	// 				))
-	// 			)
-	// 		)),
-	// 		mapTo(lightIds),
-	// 	)
-	// ))
 )
+
+// const ignoreIfLockedLightIds = ({
+// 	state$,
+// }) => (
+// 	mergeMap((
+// 		lightIds,
+// 	) => (
+// 		of(
+// 			state$
+// 			.value
+// 		)
+// 		.pipe(
+// 			map(selectLockedLightIds()),
+// 			filter((
+// 				lockedLightIds,
+// 			) => (
+// 				!(
+// 					lightIds
+// 					.some((
+// 						lightId,
+// 					) => (
+// 						lockedLightIds
+// 						.has(lightId)
+// 					))
+// 				)
+// 			)),
+// 			mapTo(lightIds),
+// 		)
+// 	))
+// )
 
 module.exports = ignoreIfLockedLightIds
