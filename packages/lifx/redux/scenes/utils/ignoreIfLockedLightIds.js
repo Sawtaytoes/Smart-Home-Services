07b1d0a142b91df15846417efdc372e5c5d0fa99
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-24 01:40:12 -0500

    Stopped ignoring commands if those lightIds were locked

--- packages/lifx/redux/scenes/utils/ignoreIfLockedLightIds.js
+++ packages/lifx/redux/scenes/utils/ignoreIfLockedLightIds.js
@@ -6,31 +6,33 @@ const { selectLockedLightIds } = require('$redux/lights/selectors')
 const ignoreIfLockedLightIds = ({
 	state$,
 }) => (
-	mergeMap((
-		lightIds,
-	) => (
-		of(
-			state$
-			.value
-		)
-		.pipe(
-			map(selectLockedLightIds()),
-			filter((
-				lockedLightIds,
-			) => (
-				!(
-					lightIds
-					.some((
-						lightId,
-					) => (
-						lockedLightIds
-						.has(lightId)
-					))
-				)
-			)),
-			mapTo(lightIds),
-		)
-	))
+	// TEMP filter. Might end up keeping it like this because commenting this out will give more immediate feedback.
+	filter(Boolean)
+	// mergeMap((
+	// 	lightIds,
+	// ) => (
+	// 	of(
+	// 		state$
+	// 		.value
+	// 	)
+	// 	.pipe(
+	// 		map(selectLockedLightIds()),
+	// 		filter((
+	// 			lockedLightIds,
+	// 		) => (
+	// 			!(
+	// 				lightIds
+	// 				.some((
+	// 					lightId,
+	// 				) => (
+	// 					lockedLightIds
+	// 					.has(lightId)
+	// 				))
+	// 			)
+	// 		)),
+	// 		mapTo(lightIds),
+	// 	)
+	// ))
 )
 
 module.exports = ignoreIfLockedLightIds
