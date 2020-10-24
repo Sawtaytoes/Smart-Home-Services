8e45ff1d07fc79af40e4f6cac672c5faf5d305fa
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-23 23:22:55 -0500

    Fixed comma-dangle ESLint rule problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
-const { resolve } = require('path')
-
 module.exports = {
 	extends: [
 		'@ghadyani-eslint/node',
 	],
+	rules: {
+		'comma-dangle': [
+			'warn',
+			{
+				arrays: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'only-multiline',
+				imports: 'always-multiline',
+				objects: 'always-multiline',
+			},
+		],
+	},
 }
--- packages/flic/.eslintrc.js
+++ packages/flic/.eslintrc.js
@@ -4,12 +4,24 @@ module.exports = {
 	extends: [
 		'@ghadyani-eslint/node',
 	],
+	rules: {
+		'comma-dangle': [
+			'warn',
+			{
+				arrays: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'only-multiline',
+				imports: 'always-multiline',
+				objects: 'always-multiline',
+			},
+		],
+	},
 	settings: {
 		'import/resolver': {
 			alias: [
 				['$redux', resolve(__dirname, 'redux')],
 				['$utils', resolve(__dirname, 'utils')],
 			],
-		}
+		},
 	},
 }
