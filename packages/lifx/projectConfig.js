f762afa3b47887a932af052c64424d8f2f13808c
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-24 01:39:46 -0500

    Removed lifxLanClient config values

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
 	applicationName: 'lifxController',
 	lifxApiAddress: 'https://api.lifx.com/v1/',
-	lifxLanClient: {
-		messageHandlerTimeout: 500,
-		resendMaxTimes: 3,
-	},
+	// TEMP. This is not working as expected. Seems to be not waiting long enough.
+	// lifxLanClient: {
+	// 	messageHandlerTimeout: 500,
+	// 	resendMaxTimes: 3,
+	// },
 }
