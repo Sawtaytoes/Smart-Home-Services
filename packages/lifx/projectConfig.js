1ebf05c85916fd64c790fafa6d3bf6ffb6db0176
Kevin Ghadyani <Saturn2888@gmail.com>
2020-10-24 22:53:45 -0500

    Set timeout to 50ms instead of 500ms

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -2,7 +2,7 @@ module.exports = {
 	applicationName: 'lifxController',
 	lifxApiAddress: 'https://api.lifx.com/v1/',
 	lifxLanClient: {
-		messageHandlerTimeout: 500,
+		messageHandlerTimeout: 50,
 		resendMaxTimes: 20,
 	},
 }
