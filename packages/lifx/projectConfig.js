51f5269e40dc43bbfb7c812c2120d4d7cfa1fbf7
Kevin Ghadyani <Saturn2888@gmail.com>
2020-11-03 22:02:55 -0600

    Removed double-forward-slash in all LIFX API requests

--- packages/lifx/projectConfig.js
+++ packages/lifx/projectConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
 	applicationName: 'lifxController',
-	lifxApiAddress: 'https://api.lifx.com/v1/',
+	lifxApiAddress: 'https://api.lifx.com/v1',
 	lifxLanClient: {
 		messageHandlerTimeout: 500,
 		resendMaxTimes: 20,
